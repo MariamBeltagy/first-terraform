4f5603b1127783e417878e4d79f3e2cc9408348f
MariamBeltagy <maeraim2@gmail.com>
2022-09-08 14:01:16 +0200

    vnet & subnets

--- main.tf
+++ main.tf
@@ -17,6 +17,25 @@ resource "azurerm_resource_group" "tf_test2" {
   name     = "rg2"
   location = "centralus"
 }
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.tf_test2.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.tf_test2]
+}
 
 # resource "azurerm_container_group" "cg_test" {
 #   name                = "cg1"
