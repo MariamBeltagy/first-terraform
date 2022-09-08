ad9cc6ae0f591aec2150af0d24cba7abf035fc6b
MariamBeltagy <maeraim2@gmail.com>
2022-09-08 14:08:15 +0200

    rg changes

--- main.tf
+++ main.tf
@@ -13,13 +13,13 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "tf_test2" {
-  name     = "rg2"
+resource "azurerm_resource_group" "tf_test" {
+  name     = "rg1"
   location = "centralus"
 }
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.tf_test2.name
+  resource_group_name = azurerm_resource_group.tf_test.name
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -34,7 +34,7 @@ module "vnet" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.tf_test2]
+  depends_on = [azurerm_resource_group.tf_test]
 }
 
 # resource "azurerm_container_group" "cg_test" {
